1f0f77e7a79ee7fb3b38c9a53b91b3202c80931d
Sweet T <thurtt@rocketmail.com>
2020-05-15 16:09:07 -0700

    Added initial routes to <Home />,  <Players />, and  <Teams /> in App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,7 +1,17 @@
 import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Home from './Home';
+import Players from './Players';
+import Teams from './Teams';
 
 function App() {
-  return <div className='App'>Basketball League</div>;
+  return (
+    <Router>
+      <Route to={'/'} component={Home} />
+      <Route to={'/players'} component={Players} />
+      <Route to={'/teams'} component={Teams} />
+    </Router>
+  );
 }
 
 export default App;
