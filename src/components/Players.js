97790dcd606acfa9d427540f9ded74e4f23e2b71
Sweet T <thurtt@rocketmail.com>
2020-05-17 12:10:58 -0700

    Built out fetchPlayers logic and implemented <Sidebar /> in <Players /> also handles players with url querys for each team

--- src/components/Players.js
+++ src/components/Players.js
@@ -1,7 +1,48 @@
 import React, { Component } from 'react';
+import Sidebar from './Sidebar';
+import { getPlayers } from '../api';
+import { parse } from 'query-string';
+import { Route, Link } from 'react-router-dom';
+import slug from 'slug';
 
 export default class Players extends Component {
+  state = {
+    players: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    const { location } = this.props;
+    location.search
+      ? this.fetchPlayers(parse(location.search).teamId)
+      : this.fetchPlayers();
+  }
+
+  fetchPlayers = (teamId) => {
+    getPlayers(teamId).then((players) => {
+      this.setState({
+        loading: false,
+        players,
+      });
+    });
+  };
+
   render() {
-    return <div className='container two-column'>Players</div>;
+    const { players, loading } = this.state;
+    const { match, location } = this.props;
+    console.log(players);
+    return (
+      <div className='container two-column'>
+        <Sidebar
+          list={players.map((player) => player.name)}
+          loading={loading}
+          title='Players'
+          {...this.props}
+        />
+        {!loading && location.pathname === '/players' && (
+          <div className='sidebar-instruction'>Select a player</div>
+        )}
+      </div>
+    );
   }
 }
