4b9ae59e016b1abbf3c2adf7584d3f6ed9f2d02b
Sweet T <thurtt@rocketmail.com>
2020-05-17 16:15:33 -0700

    Set up sidebar and fetched teamNames for <Teams />

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,7 +1,28 @@
 import React, { Component } from 'react';
+import Sidebar from './Sidebar';
+import { getTeamNames } from '../api';
 
 export default class Teams extends Component {
+  state = {
+    teams: [],
+    loading: true,
+  };
+
+  componentDidMount() {
+    getTeamNames().then((teams) =>
+      this.setState({
+        loading: false,
+        teams,
+      })
+    );
+  }
+
   render() {
-    return <div className='container two-column'>Teams</div>;
+    const { teams, loading } = this.state;
+    return (
+      <div className='container two-column'>
+        <Sidebar list={teams} loading={loading} title='Teams' {...this.props} />
+      </div>
+    );
   }
 }
