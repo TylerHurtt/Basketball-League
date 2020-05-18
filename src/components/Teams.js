428d5fa9effc7d5f8385dc887a4b41a855742bd4
Sweet T <thurtt@rocketmail.com>
2020-05-17 18:08:10 -0700

    Added <Sidebar /> with teamNames to <Teams /> fetched data with getTeamNames from api.js

--- src/components/Teams.js
+++ src/components/Teams.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import Sidebar from './Sidebar';
-import { getTeamNames } from '../api';
+import { getTeamNames, getTeam } from '../api';
+import { Route } from 'react-router-dom';
+import { parse } from 'query-string';
 
 export default class Teams extends Component {
   state = {
@@ -19,9 +21,15 @@ export default class Teams extends Component {
 
   render() {
     const { teams, loading } = this.state;
+    const { location, match } = this.props;
+
     return (
       <div className='container two-column'>
         <Sidebar list={teams} loading={loading} title='Teams' {...this.props} />
+        {!loading && location.pathname === '/teams' && (
+          <div className='sidebar-instruction'>Select a team</div>
+        )}
+        />
       </div>
     );
   }
