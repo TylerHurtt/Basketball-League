dd5c044e42013981279ec62ba67407da1e85f66b
Sweet T <thurtt@rocketmail.com>
2020-05-19 12:24:14 -0700

    Used React.lazy and React.suspense to code split app

--- src/components/App.js
+++ src/components/App.js
@@ -1,30 +1,33 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Nav from './Nav';
-import Home from './Home';
-import Players from './Players';
-import Teams from './Teams';
-import TeamPage from './TeamPage';
-import Articles from './Articles';
+import Loading from './Loading';
+const Home = React.lazy(() => import('./Home'));
+const Players = React.lazy(() => import('./Players'));
+const Teams = React.lazy(() => import('./Teams'));
+const TeamPage = React.lazy(() => import('./TeamPage'));
+const Articles = React.lazy(() => import('./Articles'));
 
 function App() {
   return (
     <Router>
       <Nav />
 
-      <Switch>
-        <Route exact path={'/'} component={Home} />
-        <Route path={'/players'} component={Players} />
-        <Route path={'/teams'} component={Teams} />
-        <Route exact path={'/:teamId'} component={TeamPage} />
-        <Route path={'/:teamId/articles'} component={Articles} />
+      <React.Suspense fallback={Loading}>
+        <Switch>
+          <Route exact path={'/'} component={Home} />
+          <Route path={'/players'} component={Players} />
+          <Route path={'/teams'} component={Teams} />
+          <Route exact path={'/:teamId'} component={TeamPage} />
+          <Route path={'/:teamId/articles'} component={Articles} />
 
-        <Route
-          render={() => (
-            <h1 className='text-center'>{'Uh-Oh... Four Oh Four'}</h1>
-          )}
-        />
-      </Switch>
+          <Route
+            render={() => (
+              <h1 className='text-center'>{'Uh-Oh... Four Oh Four'}</h1>
+            )}
+          />
+        </Switch>
+      </React.Suspense>
     </Router>
   );
 }
