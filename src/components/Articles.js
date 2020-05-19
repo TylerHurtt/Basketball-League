45fa2027320e10e0322342cad550e36cebf4f34a
Sweet T <thurtt@rocketmail.com>
2020-05-18 18:58:37 -0700

    Finished UI for Article

--- src/components/Articles.js
+++ src/components/Articles.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react';
 import Sidebar from './Sidebar';
 import { getTeamsArticles } from '../api';
 import { Route } from 'react-router-dom';
+import Article from './Article';
 
 export default class Articles extends Component {
   state = {
@@ -22,6 +23,9 @@ export default class Articles extends Component {
   render() {
     const { articles, loading } = this.state;
     const { match } = this.props;
+    const { url } = match;
+    const { teamId } = match.params;
+
     return loading ? (
       <h1 className='center'>Loading...</h1>
     ) : (
@@ -32,7 +36,26 @@ export default class Articles extends Component {
           title='Articles'
           {...this.props}
         />
-        <Route path={`${match.url}/:articleId`} />
+        <Route
+          path={`${url}/:articleId`}
+          render={({ match }) => (
+            <Article id={match.params.articleId} teamId={teamId}>
+              {(article) =>
+                !article ? (
+                  <h1 className='center'>Loading...</h1>
+                ) : (
+                  <div className='panel'>
+                    <article className='article' key={article.id}>
+                      <h1 className='header'>{article.title}</h1>
+                      <h5>{article.date.toLocaleDateString()}</h5>
+                      <p>{article.body}</p>
+                    </article>
+                  </div>
+                )
+              }
+            </Article>
+          )}
+        />
       </div>
     );
   }
